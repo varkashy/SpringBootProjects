c8072f0748b1341d2a54c0b15e2857b7b05de1ec
varkashy <varunkkashyap@gmail.com>
2018-08-19 17:15:25 -0500

    added logic for getting file inline or attachment

--- simpleapproach/src/main/java/com/varkashy/simpleapproach/pdf/PdfService.java
+++ simpleapproach/src/main/java/com/varkashy/simpleapproach/pdf/PdfService.java
@@ -1,5 +1,8 @@
 package com.varkashy.simpleapproach.pdf;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,5 +28,30 @@ public class PdfService {
             e.printStackTrace();
         }
         return "hello world".getBytes();
+    }
+
+    @GetMapping(path = "/pdffile")
+    public ResponseEntity<byte[]> generatePdfResponseEntity(){
+        HttpHeaders httpHeaders=new HttpHeaders();
+        httpHeaders.add("Cache-Control", "no-cache, no-store, must-revalidate");
+        httpHeaders.add("Pragma", "no-cache");
+        httpHeaders.add("Expires", "0");
+        //httpHeaders.add("content-disposition","inline;filename=sample.pdf");
+        httpHeaders.add("content-disposition","attachment;filename=sample.pdf");
+        httpHeaders.add("content-type","application/pdf;charset=utf-8");
+        byte[] dataInByte=null;
+        try {
+            FileInputStream fis = new FileInputStream(new File(this.getClass().getResource( "/helloWorld.pdf" ).toURI()));
+            dataInByte= IOUtils.toByteArray(fis);
+        }
+        catch(IOException ex){
+            System.out.println(ex);
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+        if(dataInByte!=null)
+            return new ResponseEntity<>(dataInByte,httpHeaders,HttpStatus.OK);
+        else
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
